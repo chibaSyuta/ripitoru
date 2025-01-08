253aa652bab6c429cb576e6f29e86e7ca73e8de7
しゅうた <53383746+chibaSyuta@users.noreply.github.com>
2025-01-09 01:33:29 +0900

    Add files via upload

--- LPメイン/main.js
+++ LPメイン/main.js
@@ -7,23 +7,32 @@
     let imageContainer = document.querySelectorAll('.imageContainer');
     let flex2 = document.querySelectorAll('.flex2');
     // imageContainer.style.height = `${flex2.offsetHeight}px`;
+    
 
     flex2.forEach( (flex2) => {        
         flex2.children[0].style.height = `${flex2.offsetHeight}px`;
         flex2.children[0].style.width = `${flex2.offsetHeight}px`;
     });
 
-        
     window.addEventListener('resize', () => {
         if ( window.innerWidth >= 700 ) {
-            console.log( 112 );
             flex2.forEach( (flex2) => {        
                 flex2.children[0].style.height = `${flex2.offsetHeight}px`;
                 flex2.children[0].style.width = `${flex2.offsetHeight}px`;
             });
         }
+        const width = attention.offsetWidth;
+        items.style.maxWidth = `${width}px`;
     });
 
+
+    let attention = document.querySelector('.attention');
+    let items = document.querySelector('.items');
+    window.addEventListener('resize', () => {
+        console.log( "aaa" );
+        const width = attention.offsetWidth;
+        items.style.maxWidth = `${width}px`;
+    });
 
 
 }
